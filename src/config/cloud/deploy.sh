ba84730b1c6928eb24471f37ddb9e22c9841223b
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2016-12-14 04:31:19 +0000

    [DNM] ipxe shell for debugging on gcp
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

---
+++ src/config/cloud/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# DO NOT MERGE!
+set -e
+set -x
+
+make bin/ipxe.usb CONFIG=cloud EMBED=config/cloud/shell.ipxe
+cp -f bin/ipxe.usb /tmp/disk.raw
+( cd /tmp; tar Sczvf ipxe-shell-image.tar.gz disk.raw )
+gsutil cp /tmp/ipxe-shell-image.tar.gz gs://suda-main
+
+gcloud compute instances delete -q ipxe01 || true
+gcloud compute images delete -q ipxe-shell || true
+gcloud compute images create ipxe-shell --source-uri gs://suda-main/ipxe-shell-image.tar.gz
+gcloud compute instances create ipxe01 --image ipxe-shell --metadata serial-port-enable=1
+
+gcloud beta compute connect-to-serial-port ipxe01
